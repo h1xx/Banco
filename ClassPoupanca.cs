2830b0122197ea9fe1849a9fc628e07e09987714
André Victor Leal <100169443+h1xx@users.noreply.github.com>
2022-11-03 19:24:25 -0300

    Add files via upload
    Class banco poupança

---
+++ ClassPoupanca.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class ClassPoupanca
+    {
+        public double SaldoPoupanca { get; set; }
+        public double Saldo { get; set; }
+        public DateTime dataDeposito { get; set; }
+
+        public void DepositoPoupanca()
+        {
+            SaldoPoupanca = 0;
+            Saldo = 15000;
+            double SaldoTransfPoupanca = 0;
+            Console.WriteLine($"Saldo {Saldo}");
+            Console.WriteLine("Valor para depósito: ");
+            SaldoTransfPoupanca = Convert.ToDouble(Console.ReadLine());
+            var menuOption = true;
+            while (menuOption)
+            {
+                if (SaldoTransfPoupanca > Saldo)
+                {
+                    Console.WriteLine("Saldo insuficiente, tente novamente!");
+                    break;
+                }
+                else if (SaldoTransfPoupanca <= Saldo)
+                {
+                    Saldo = Saldo - SaldoTransfPoupanca;
+                    SaldoPoupanca = SaldoTransfPoupanca;
+                    dataDeposito = DateTime.Now;
+                    Console.WriteLine($"Valor depositado com sucesso! Seu novo saldo de poupança é de R${SaldoPoupanca}\nSaldo conta: R${Saldo}");
+                    menuOption = false;
+                }
+            }
+        }
+        public void ResgatePoupanca()
+        {
+            double juros = 0;
+            double taxajuros = 0.0216666666666667;
+            double ResgatePoupanca = 0;
+            var menuOption = true;
+            Console.WriteLine(SaldoPoupanca);
+            Console.WriteLine("Valor que deseja resgatar: ");
+            ResgatePoupanca = Convert.ToDouble(Console.ReadLine());
+            while (menuOption)
+            {
+                if (ResgatePoupanca > SaldoPoupanca)
+                {
+                    Console.WriteLine("Saldo insuficiente, tente novamente!");
+                    break;
+                }
+                else if (ResgatePoupanca <= SaldoPoupanca)
+                {
+                    SaldoPoupanca = SaldoPoupanca - ResgatePoupanca;
+                    Saldo = ResgatePoupanca + Saldo;
+                    DateTime dataResgate = DateTime.Now;
+                    long dataJuros = 0;
+                    dataJuros = (dataResgate.DayOfYear - dataDeposito.DayOfYear);
+                    juros = taxajuros * Convert.ToInt32(dataJuros);
+                    Saldo = Saldo + juros;
+                    Console.WriteLine($"Seu saldo é de R${Saldo} e você recebeu um juros de R${juros}");
+                }
+            }
+        }
+    }
+}
